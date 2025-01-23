630b47b6cf38aa3f72b878c7ee553959bc0b785c
janwvjaarsveld <janwvjaarsveld@gmail.com>
2025-01-23 14:56:44 +0100

    fix: update telescope method names

--- lua/telescope/_extensions/neovimcraft.lua
+++ lua/telescope/_extensions/neovimcraft.lua
@@ -1,6 +1,6 @@
 return require("telescope").register_extension({
 	exports = {
-		search_plugins = require("neovimcraft").search_plugins,
-		search_tags = require("neovimcraft").search_tags,
+		plugins = require("neovimcraft").search_plugins,
+		tags = require("neovimcraft").search_tags,
 	},
 })
